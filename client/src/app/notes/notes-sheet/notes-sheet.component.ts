69212c39bf392f8c9e1252aa99eed585d95400af
Bogomaz Dmitry <d.bogomaz@itransition.com>
2022-12-08 15:12:02 +0300

    Added filters and sorting to the sheet

--- client/src/app/notes/notes-sheet/notes-sheet.component.ts
+++ client/src/app/notes/notes-sheet/notes-sheet.component.ts
@@ -1,9 +1,8 @@
 import {
   Component,
   ViewChild,
-  OnChanges,
-  SimpleChanges,
   OnInit,
+  AfterViewInit,
 } from '@angular/core';
 import { NoteService } from '../../shared/services/note.service';
 import { Note } from '../../shared/models/note.model';
@@ -17,10 +16,10 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
   templateUrl: './notes-sheet.component.html',
   styleUrls: ['./notes-sheet.component.scss'],
 })
-export class NotesSheetComponent implements OnInit, OnChanges {
+export class NotesSheetComponent implements OnInit {
 
   public notes: Note[];
-  public displayedColumns: string[] = ['text', 'title', 'color', 'controls'];
+  public displayedColumns: string[] = ['title', 'text', 'color', 'controls'];
   public dataSource: MatTableDataSource<Note>;
 
   @ViewChild(MatSort) sort: MatSort;
@@ -35,24 +34,34 @@ export class NotesSheetComponent implements OnInit, OnChanges {
       this.notes = notes;
       this.dataSource = new MatTableDataSource(this.notes);
       this.dataSource.sort = this.sort;
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        switch (property) {
+          case 'text': return item.noteText?.text;
+          case 'title': return item.noteText?.title;
+          case 'color': return item.noteDesign?.color ?? '';
+          default: return item.order;
+        }
+      };
+      this.dataSource.filterPredicate = (n, filterValue) => {
+        filterValue = filterValue.trim().toLowerCase();
+        return !!n.noteText.text && n.noteText.text.toLowerCase().includes(filterValue)
+          || !!n.noteText.title && n.noteText.title.toLowerCase().includes(filterValue)
+          || !!n.noteDesign?.color && n.noteDesign?.color.toLowerCase().includes(filterValue);
+      };
     });
   }
 
   public announceSortChange(sortState: Sort) {
+    console.error('call announceSortChange');
     if (sortState.direction) {
-      this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+      this.liveAnnouncer.announce(`Отсортировано по ${sortState.direction}`);
     } else {
-      this.liveAnnouncer.announce('Sorting cleared');
+      this.liveAnnouncer.announce('Не отвортировано');
     }
   }
 
-  public ngOnChanges(changes: SimpleChanges): void {
-    this.notes = changes.notes.currentValue;
-    this.dataSource = new MatTableDataSource(this.notes);
-    this.dataSource.sort = this.sort;
-  }
-
-  public getNotes(): Note[] {
-    return this.noteService.notes;
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue;
   }
 }
--- client/src/app/notes/notes.module.ts
+++ client/src/app/notes/notes.module.ts
@@ -1,3 +1,5 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { NotificationsModule } from './../notifications/notifications.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,7 +9,6 @@ import { NotesComponent } from './notes.component';
 import { NoteDialogComponent } from './note-dialog/note-dialog.component';
 import { NotesSheetComponent } from './notes-sheet/notes-sheet.component';
 import { RouterModule } from '@angular/router';
-
 @NgModule({
   declarations: [
     NoteComponent,
@@ -15,7 +16,9 @@ import { RouterModule } from '@angular/router';
     NoteDialogComponent,
     NotesSheetComponent,
   ],
-  imports: [CommonModule, SharedModule, NotificationsModule, RouterModule],
+  imports: [
+    BrowserModule, BrowserAnimationsModule,
+    CommonModule, SharedModule, NotificationsModule, RouterModule, BrowserModule],
   entryComponents: [NoteDialogComponent],
 })
-export class NotesModule {}
+export class NotesModule { }
